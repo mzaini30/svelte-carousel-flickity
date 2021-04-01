0cb5a0e235ccd33e1cce3f2c25b03a907aee0f7b
mzaini30 <muhzaini30@gmail.com>
2021-04-01 14:31:16 +0800

    upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,4 @@
+<h1>Carousel A</h1>
 <Carousel>
   <div class="isi a">1</div>
   <div class="isi b">2</div>
@@ -5,15 +6,28 @@
   <div class="isi d">4</div>
   <div class="isi e">5</div>
 </Carousel>
+<h1>Carousel B</h1>
+<Carousel>
+  {#each kode as item, index}
+    <div class="full"><img src="https://http.cat/{item}" alt=""></div>
+  {/each}
+</Carousel>
 
 <script>
   import Carousel from '/src/lib/Carousel.svelte'
+  const kode = [101, 200, 404, 500, 403]
 </script>
 
 <style lang="scss">
+  .full {
+    width: 100%;
+    img {
+      width: inherit;
+    }
+  }
   .isi {
     $padding: 30px;
-    width: 100%; /* important */
+    width: 50%; /* important */
     text-align: center;
     padding-top: $padding;
     padding-bottom: $padding;
--- src/lib/Carousel.svelte
+++ src/lib/Carousel.svelte
@@ -12,7 +12,5 @@
 			contain: true
 		})
 	}
-	onMount(() => {
-		panggilFlickity()
-	})
+	onMount(() => panggilFlickity())
 </script>
