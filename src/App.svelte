76b756423744e74441b419b7c11e73aefce2629d
mzaini30 <muhzaini30@gmail.com>
2021-04-01 12:05:58 +0800

    upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,36 @@
-<h1>Flickity 1</h1>
-<p>Halo...</p>
-<Carousel {isi} {banyak}></Carousel>
+<Carousel>
+  <div class="isi a">1</div>
+  <div class="isi b">2</div>
+  <div class="isi c">3</div>
+  <div class="isi d">4</div>
+  <div class="isi e">5</div>
+</Carousel>
 
 <script>
   import Carousel from '/src/lib/Carousel.svelte'
-  const isi = [1, 2, 3, 4, 5]
-  const banyak = 2
 </script>
+
+<style lang="scss">
+  .isi {
+    $padding: 30px;
+    width: 100%; /* important */
+    text-align: center;
+    padding-top: $padding;
+    padding-bottom: $padding;
+    &.a {
+      background: red;
+    }
+    &.b {
+      background: yellow;
+    }
+    &.c {
+      background: green;
+    }
+    &.d {
+      background: blue;
+    }
+    &.e {
+      background: magenta;
+    }
+  }
+</style>
--- src/lib/Carousel.svelte
+++ src/lib/Carousel.svelte
@@ -1,14 +1,10 @@
 <!-- Flickity HTML init -->
-<div class="gallery js-flickity" bind:this={elFlickity}>
-	{#each isi as item, index}
-  		<div class="gallery-cell" style="width: {100 / banyak}%">{item}</div>
-	{/each}
+<div bind:this={elFlickity}>
+	<slot></slot>
 </div>
 
 <script>
 	let elFlickity
-	export let isi = [1, 2, 3]
-	export let banyak = 1
 	import {onMount} from 'svelte'
 	function panggilFlickity(){
 		new Flickity(elFlickity, {
@@ -20,36 +16,3 @@
 		panggilFlickity()
 	})
 </script>
-
-<style>
-	/* external css: flickity.css */
-
-	* {
-	  -webkit-box-sizing: border-box;
-	  box-sizing: border-box;
-	}
-
-	/*body { font-family: sans-serif; }*/
-
-	.gallery {
-	  /*background: #EEE;*/
-	}
-
-	.gallery-cell {
-	  /*width: 66%;*/
-	  /*height: 200px;
-	  margin-right: 10px;
-	  background: #8C8;*/
-	  /*counter-increment: gallery-cell;*/
-	}
-
-	/* cell number */
-	.gallery-cell:before {
-	  /*display: block;
-	  text-align: center;*/
-	  /*content: counter(gallery-cell);*/
-	 /* line-height: 200px;
-	  font-size: 80px;
-	  color: white;*/
-	}
-</style>
